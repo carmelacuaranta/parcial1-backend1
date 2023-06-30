2baf04b9322d59d4a0263152cb204b9bbc73f6fb
Carmela Cuaranta <carmelalucuaranta@gmail.com>
2023-06-30 20:44:07 -0300

    final

--- api-examen/api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-examen/api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name="api-movie",url = "http://localhost:8085")
@@ -25,6 +26,45 @@ public interface MovieServiceClient {
 		private String genre;
 
 		private String urlStream;
+	}
+
+	@Retry(name = "retryGetMovie")
+	@CircuitBreaker(name = "getMovieByGenre", fallbackMethod = "getMovieByGenreFallBack")
+	private void getMovieByGenre(MovieDTO entity) {
+
+		entity.getMovieByGenre(MovieDto.DocumentRequest.builder()
+				.documentType(entity.getDocumentType())
+				.documentValue(entity.getDocumentNumber())
+				.build());
+	}
+
+	public void getMovieByGenre(MovieDto entity, Throwable t) throws Exception {
+		throw new Exception("Movies not found");
+	}
+
+	@GetMapping("/api/v1/series/{genre}")
+	List<SerieDto> getSerieByGenre(@PathVariable (value = "genre") String genre);
+
+	@Getter
+	@Setter
+	class SerieDto{
+		private String id;
+		private String name;
+		private String genre;
+	}
+
+	@Retry(name = "retryGetSerie")
+	@CircuitBreaker(name = "getSerieByGenre", fallbackMethod = "getSerieByGenreFallBack")
+	private void getMovieByGenre(SerieDTO entity) {
+
+		entity.getSerieByGenre(MovieDto.DocumentRequest.builder()
+				.documentType(entity.getDocumentType())
+				.documentValue(entity.getDocumentNumber())
+				.build());
+	}
+
+	public void getSerieByGenre(MovieDto entity, Throwable t) throws Exception {
+		throw new Exception("No series found");
 	}
 
 }
